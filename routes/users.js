a4afcdd7d39f10fa00d967f53cb4570f82f2ec49
sunghyunpark <sunghyun1038@naver.com>
2018-06-20 23:16:09 +0900

    post test

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,7 @@ conn.connect();
  * 회원가입 시 uid, nickName, loginType을 받아와 db에 저장한다.
  */
 router.post('/register', function(req, res){
+  console.log(req.body.uid);
   var uid = req.body.uid;
   var nickName = req.body.nickName;
   var loginType = req.body.loginType;
