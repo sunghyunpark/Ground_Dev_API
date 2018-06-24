a021566a9c7759b3ebddf79cdf326e035be5f0d1
sunghyunpark <sunghyun1038@naver.com>
2018-06-24 22:56:35 +0900

    mysql error rollback

--- routes/users.js
+++ routes/users.js
@@ -5,36 +5,13 @@ var mysql = require('mysql');
 var router = express.Router();
 
 var mysql = require('mysql');
-var db_config = mysql.createConnection({
+var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'qkr103838!@',
   database : 'ground_dev'
 });
-var conn;
-
-function handleDisconnect() {
-  conn = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  conn.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  conn.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
+conn.connect();
 
 /*
  * 회원가입 시 uid, nickName, loginType을 받아와 db에 저장한다.
