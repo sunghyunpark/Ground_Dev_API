47da545c3842e2014a1c76430126d8fe8f975620
sunghyunpark <sunghyun1038@naver.com>
2018-07-17 13:17:20 +0900

    add my/article, my/comment

--- routes/my.js
+++ routes/my.js
@@ -15,14 +15,83 @@ conn.connect();
 /*
 * 내가 쓴 글 match / hire / recruit
 */
-router.get('/article/:boardType/:uid', function(req, res){
-  
+router.get('/article/:boardType/:uid/:no', function(req, res){
+  var boardType = req.params.boardType;
+  var uid = req.params.uid;
+  var no = req.params.no;
+  var tableName;
+
+  if(boardType == 'match'){
+    tableName = 'MBoard';
+  }else if(boardType == 'hire'){
+    tableName = 'HBoard';
+  }else if(boardType == 'recruit'){
+    tableName = 'RBoard';
+  }
+
+  var offsetSql = 'AND a.created_at < (SELECT created_at FROM '+tableName+' WHERE no=?)';
+  if(no == 0){
+    offsetSql = '';
+  }
+  var sql = 'SELECT a.no, a.board_type, a.area_no, a.writer_id, a.title, a.contents, a.blocked, a.view_cnt, a.comment_cnt, a.created_at, b.nick_name FROM '+
+  tableName+' AS a JOIN users AS b ON(a.writer_id=b.uid) WHERE a.writer_id=? '+offsetSql+' ORDER BY a.created_at DESC LIMIT 10';
+
+  conn.query(sql, [uid, no], function(err, result, fields){
+    if(err){
+      console.log(err);
+      res.json({
+        code : 500,
+        message : 'Internal Server Error'
+      });
+    }else{
+      res.json({
+        code : 200,
+        message : 'Success',
+        result : result
+      });
+    }
+  })
 })
 
 /*
 * 내가 쓴 댓글 match / hire / recruit
 */
 router.get('/comment/:boardType/:uid', function(req, res){
+  var boardType = req.params.boardType;
+  var uid = req.params.uid;
+  var no = req.params.no;
+  var tableName;
+
+  if(boardType == 'match'){
+    tableName = 'MComment';
+  }else if(boardType == 'hire'){
+    tableName = 'HComment';
+  }else if(boardType == 'recruit'){
+    tableName = 'RComment';
+  }
+
+  var offsetSql = 'AND a.created_at < (SELECT created_at FROM '+tableName+' WHERE no=?)';
+  if(no == 0){
+    offsetSql = '';
+  }
+  var sql = 'SELECT a.no, a.article_no, a.area_name, a.writer_id, a.comment, a.blocked, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+tableName+
+  ' AS a JOIN users AS b ON(a.writer_id = b.uid) WHERE a.writer_id=? '+offsetSql+' ORDER BY a.created_at DESC LIMIT 10';
+
+  conn.query(sql, [uid, no], function(err, result, fields){
+    if(err){
+      console.log(err);
+      res.json({
+        code : 500,
+        message : 'Internal Server Error'
+      });
+    }else{
+      res.json({
+        code : 200,
+        message : 'Success',
+        result : result
+      });
+    }
+  })
 
 })
 
