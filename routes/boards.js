a0870d2572dd0570a38d7ffb6a8c57387e57b725
sunghyunpark <sunghyun1038@naver.com>
2018-07-28 16:01:54 +0900

    api test

--- routes/boards.js
+++ routes/boards.js
@@ -625,7 +625,7 @@ router.get('/:boardType/recent/:no', function(req, res){
   console.log(articleNo);
   console.log(offsetSql+"123");
 
-  var sql = 'SELECT * FROM '+tableName+' AS a'+offsetSql+' ORDER BY a.created_at DESC LIMIT '+limit;
+  var sql = 'SELECT * FROM '+tableName+' AS a'+' ORDER BY a.created_at DESC LIMIT '+limit;
   conn.query(sql, [articleNo], function(err, result, fields){
     if(err){
       console.log(err);
