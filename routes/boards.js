ed14b0286b649c2925d09b9e01fefbf15a6a48c1
sunghyunpark <sunghyun1038@naver.com>
2018-08-15 19:14:06 +0900

    recent article list api

--- routes/boards.js
+++ routes/boards.js
@@ -702,7 +702,8 @@ router.get('/recent/:boardType/:no/:limit', function(req, res){
     offsetSql = ' WHERE a.created_at < (SELECT created_at FROM '+tableName+' WHERE no=?)';
   }
 
-  var sql = 'SELECT * FROM '+tableName+' AS a'+offsetSql+' ORDER BY a.created_at DESC LIMIT '+limit;
+  var sql = 'SELECT a.no, a.board_type, a.area_no, a.writer_id, a.title, a.contents, a.match_state, a.blocked, a.view_cnt, a.comment_cnt, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+
+  tableName+' AS a JOIN users AS b ON(a.writer_id=b.uid)'+offsetSql+' ORDER BY a.created_at DESC LIMIT '+limit;
   conn.query(sql, [articleNo], function(err, result, fields){
     if(err){
       console.log(err);
