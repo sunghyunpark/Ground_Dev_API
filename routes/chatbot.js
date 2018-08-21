aadb52e9114f5541bdeaca59e5d74a061ea047b0
sunghyunpark <sunghyun1038@naver.com>
2018-08-21 16:56:34 +0900

    kakao chatbot test

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -13,12 +13,51 @@ var conn = mysql.createConnection({
 });
 conn.connect();
 
+/*
 router.get('/kakao/keyboard', function(req, res){
   var data = {
     'type' : 'buttons',
     'buttons' : ['ground1', 'ground2', 'ground3']
   };
   res.json(data);
+})*/
+
+router.post('/kakao/message', function(req, res){
+  var msg = req.body.content;
+
+  var response;
+
+  switch (msg) {
+    case 'hi':
+    response = {
+      'message' : {
+        'text' : 'hi 선택'
+      }
+    }
+      break;
+
+      case 'hello':
+      reponse = {
+        'message' : {
+          'text' : 'hello 선택'
+        },
+        keyboard : {
+          'type' : 'buttons',
+          'buttons' : ['button1', 'button2']
+        }
+      }
+      break;
+
+    default:
+    response = {
+      'message' : {
+        'text' : '다시 입력해주세요.'
+      }
+    }
+      break;
+  }
+
+
 })
 
 module.exports = router;
