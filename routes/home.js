571668869fbae7bd8e01f53d5e2ec406f21609e6
sunghyunpark <sunghyun1038@naver.com>
2018-08-24 01:39:36 +0900

    api test

--- routes/home.js
+++ routes/home.js
@@ -15,6 +15,51 @@ var conn = mysql.createConnection({
 conn.connect();
 
 /*
+* 최신글 리스트를 5개씩 내려준다.(match / hire / recruit)
+*/
+router.get('/recent/:boardType/:no/:limit', function(req, res){
+  var boardType = req.params.boardType;
+  var articleNo = req.params.no;
+  var tableName;
+  var limit = req.params.limit;
+  var offsetSql;
+  var matchData = '';
+
+  if(boardType == 'match'){
+    tableName = 'MBoard';
+    matchData = 'a.match_date, a.average_age,'
+  }else if(boardType == 'hire'){
+    tableName = 'HBoard';
+  }else if(boardType == 'recruit'){
+    tableName = 'RBoard';
+  }
+  if(articleNo == 0){
+    offsetSql = '';
+  }else{
+    offsetSql = ' WHERE a.created_at < (SELECT created_at FROM '+tableName+' WHERE no=?)';
+  }
+
+  var sql = 'SELECT a.no, a.board_type, a.area_no, a.writer_id, a.title, a.contents, a.match_state, a.blocked, a.view_cnt, a.comment_cnt, '+matchData+
+  ' a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+
+  tableName+' AS a JOIN users AS b ON(a.writer_id=b.uid)'+offsetSql+' ORDER BY a.created_at DESC LIMIT '+limit;
+  conn.query(sql, [articleNo], function(err, result, fields){
+    if(err){
+      console.log(err);
+      res.json({
+        code : 500,
+        message : 'Internal Server Error Recent'
+      });
+    }else{
+      res.json({
+        code : 200,
+        message : 'Success',
+        result : result
+      });
+    }
+  })
+})
+
+/*
 * 오늘의 시합
 */
 router.get('/today/:no/:limit', function(req, res){
