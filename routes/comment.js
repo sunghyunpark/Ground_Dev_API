0fe26074d5691f398bc75242c07ea9f2e482b102
sunghyunpark <sunghyun1038@naver.com>
2018-09-17 13:03:32 +0900

    api test

--- routes/comment.js
+++ routes/comment.js
@@ -7,6 +7,7 @@ var mysql = require('mysql');
 var router = express.Router();
 var sortModule = require('../util/sortModule');
 var fcmModule = require('./push');
+var responseUtil = require('../util/responseUtil');
 
 var mysql = require('mysql');
 var conn = mysql.createConnection({
@@ -39,43 +40,23 @@ router.post('/view/comment', function(req, res){
   conn.query(sql, [articleNo, areaName, writer_id, comment, currentTime], function(err, result, fields){
     if(err){
       console.log(err);
-      res.json({
-        code : 500,
-        message : 'Internal Server Error'
-      });
-    }else{
+      res.json(responseUtil.successFalse(500, 'Internal Server Error'));
       //댓글 insert 성공 후 해당 게시글 Table에서 comment_Cnt를 +1 업데이트해준다.
       var sql = 'UPDATE '+tableNameOfArticle+' SET comment_cnt = comment_cnt +1 WHERE no=?';
       conn.query(sql, [articleNo], function(err, result, fields){
         if(err){
           console.log(err);
-          res.json({
-            code : 500,
-            message : 'Internal Server Error'
-          });
+          res.json(responseUtil.successFalse(500, 'Internal Server Error'));
         }else{
           //boardType이 match인 경우 MBoard내에서도 comnment_cnt를 업데이트해준다.
           if(boardType == 'match'){
           var sql = 'UPDATE MBoard SET comment_cnt = comment_cnt +1 WHERE no=?';
           conn.query(sql, [articleNo], function(err, result, fields){
-            if(err){
-              res.json({
-                code : 500,
-                message : 'Internal Server Error'
-              });
-            }else{
-              res.json({
-                code : 200,
-                message : 'Success'
-              });
-            }
+            res.json(err ? responseUtil.successFalse(500, 'Internal Server Error') : responseUtil.successTrue());
           })
         }else{
           //boardType이 match가 아닌 경우
-          res.json({
-            code : 200,
-            message : 'Success'
-          });
+          res.json(responseUtil.successTrue('Success'));
         }
         }
       })
@@ -119,38 +100,14 @@ router.get('/:boardType/view/:articleNo/:areaNo/commentList/:commentNo', functio
       var sql = 'SELECT a.no, a.article_no, a.area_name, a.writer_id, a.comment, a.blocked, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+tableNameOfComment+
       ' AS a JOIN users AS b ON(a.writer_id = b.uid) WHERE a.article_no=? AND a.area_name=? '+offsetSql+' ORDER BY a.created_at ASC LIMIT 10';
       conn.query(sql, [articleNo, areaName, commentNo], function(err, result, fields){
-        if(err){
-          console.log(err);
-          res.json({
-            code : 500,
-            message : 'Internal Server Error'
-          });
-        }else{
-          res.json({
-            code : 200,
-            message : 'Success',
-            result : result
-          });
-        }
+        res.json(err ? responseUtil.successFalse(500, 'Internal Server Error') : responseUtil.successTrueWithData(result));
       })
 
     }else{
       var sql = 'SELECT a.no, a.article_no, a.area_name, a.writer_id, a.comment, a.blocked, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+tableNameOfComment+
       ' AS a JOIN users AS b ON(a.writer_id = b.uid) WHERE a.article_no=? '+offsetSql+' ORDER BY a.created_at ASC LIMIT 10';
       conn.query(sql, [articleNo, commentNo], function(err, result, fields){
-        if(err){
-          console.log(err);
-          res.json({
-            code : 500,
-            message : 'Internal Server Error'
-          });
-        }else{
-          res.json({
-            code : 200,
-            message : 'Success',
-            result : result
-          });
-        }
+        res.json(err ? responseUtil.successFalse(500, 'Internal Server Error') : responseUtil.successTrueWithData(result));
       })
 
     }
@@ -170,43 +127,24 @@ router.delete('/view/comment/delete/:boardType/:no/:articleNo/:areaNo', function
   var sql = 'DELETE FROM '+tableNameOfComment+' WHERE no=?';
   conn.query(sql, [no], function(err, result, fields){
     if(err){
-      res.json({
-        code : 500,
-        message : 'Internal Server Error'
-      });
+      res.json(responseUtil.successFalse(500, 'Internal Server Error'));
     }else{
       //댓글 delete 성공 후 해당 게시글 Table에서 comment_Cnt를 -1 업데이트해준다.
       var sql = 'UPDATE '+tableNameOfArticle+' SET comment_cnt = comment_cnt -1 WHERE no=?';
       conn.query(sql, [articleNo], function(err, result, fields){
         if(err){
           console.log(err);
-          res.json({
-            code : 500,
-            message : 'Internal Server Error'
-          });
+          rres.json(responseUtil.successFalse(500, 'Internal Server Error'));
         }else{
           //boardType이 match인 경우 MBoard내에서도 comnment_cnt를 업데이트해준다.
           if(boardType == 'match'){
           var sql = 'UPDATE MBoard SET comment_cnt = comment_cnt -1 WHERE no=?';
           conn.query(sql, [articleNo], function(err, result, fields){
-            if(err){
-              res.json({
-                code : 500,
-                message : 'Internal Server Error'
-              });
-            }else{
-              res.json({
-                code : 200,
-                message : 'Success'
-              });
-            }
+            res.json(err ? responseUtil.successFalse(500, 'Internal Server Error') : responseUtil.successTrue());
           })
         }else{
           //boardType이 match가 아닌 경우
-          res.json({
-            code : 200,
-            message : 'Success'
-          });
+          res.json(responseUtil.successTrue());
         }
         }
       })
