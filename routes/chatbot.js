faf35f362351c75e37c7c985c13fe603d9f64698
sunghyunpark <sunghyun1038@naver.com>
2018-09-28 01:11:35 +0900

    chatbot test

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -87,7 +87,7 @@ router.post('/kakao/message', function(req, res){
 
   }else if(msg == '매칭'){
     responseText += '매칭 게시판의 최신글 목록입니다.\n';
-    var sql = 'SELECT title, area_no, match_state FROM MBoard WHERE ORDER BY created_at DESC LIMIT 5';
+    var sql = 'SELECT title, area_no, match_state FROM MBoard ORDER BY created_at DESC LIMIT 5';
 
     conn.query(sql, function(err, result, fields){
       if(err){
