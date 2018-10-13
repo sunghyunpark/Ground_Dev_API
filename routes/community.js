168ae21464b0c898d7aff124777048f79b32ccb2
sunghyunpark <sunghyun1038@naver.com>
2018-10-13 21:18:31 +0900

    api test

--- routes/community.js
+++ routes/community.js
@@ -95,7 +95,7 @@ router.get('/detailView/:boardType/:no', function(req, res){
   var no = req.params.no;
   var tableName = sortModule.sortTableNameOfArticle(boardType);
 
-  var sql = 'SELECT a.no, a.board_type, a.writer_id, a.title, a.contens, a.photo, a.photo_thumb, a.blocked, a.view_cnt, '+
+  var sql = 'SELECT a.no, a.board_type, a.writer_id, a.title, a.contents, a.photo, a.photo_thumb, a.blocked, a.view_cnt, '+
   'a.comment_cnt, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM '+tableName+' AS a JOIN users AS b ON(a.writer_id=b.uid)';
 
   conn.query(sql, [no], function(err, result, fields){
