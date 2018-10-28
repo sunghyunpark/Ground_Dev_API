f268e6df230e1de2014d060e511cc1d4f91484e6
sunghyunpark <sunghyun1038@naver.com>
2018-10-28 20:51:40 +0900

    api test

--- routes/search.js
+++ routes/search.js
@@ -19,7 +19,7 @@ router.get('/matchArticle/:no/:areaNoStr/:order/:matchDate',function(req, res){
   var areaNoArray = req.params.areaNoStr.split(',');
   var order = req.params.order;
   var matchDate = req.params.matchDate;
-  var offsetSql = (no == 0) ? '' : ' AND a.created_at < (SELECT created_at FROM MBoard WHERE no='+no+')';
+  var offsetSql = (no == 0) ? '' : ' AND article.created_at < (SELECT created_at FROM MBoard WHERE no='+no+')';
   var whereSql1 = 'where (area_no=?';
   var whereSql2 = '';
 
@@ -40,17 +40,31 @@ router.get('/matchArticle/:no/:areaNoStr/:order/:matchDate',function(req, res){
   if(order == 'all'){
     orderData = '';
   }else if(order == 'matchDate'){
-    orderData = ' AND a.match_date=?';
+    orderData = ' AND article.match_date=?';
     areaNoArray.push(matchDate);
   }else if(order == 'matchState'){
-    orderData = ' AND a.match_state=\'N\'';
+    orderData = ' AND article.match_state=\'N\'';
   }
 
   areaNoArray.push(no);
 
-  var sql = 'SELECT a.no, a.board_type, a.area_no, a.writer_id, a.title, a.contents, a.match_state, a.blocked, a.view_cnt, '+
-  'a.comment_cnt, a.match_date, a.average_age, a.created_at, b.nick_name, b.profile, b.profile_thumb FROM MBoard AS a '+
-  'JOIN users AS b ON(a.writer_id=b.uid) ' + whereSql1 + orderData + offsetSql + ' ORDER BY a.created_at DESC LIMIT 10';
+  var sql = 'SELECT article.no, '+
+  'article.board_type AS boardType, '+
+  'article.area_no AS areaNo, '+
+  'article.writer_id AS writerId, '+
+  'article.title, '+
+  'article.contents, '+
+  'article.match_state AS matchState, '+
+  'article.blocked, '+
+  'article.view_cnt AS viewCnt, '+
+  'article.comment_cnt AS commentCnt, '+
+  'article.match_date AS matchDate, '+
+  'article.average_age AS averageAge, '+
+  'article.created_at AS createdAt, '+
+  'users.nick_name AS nickName, '+
+  'users.profile, '+
+  'users.profile_thumb AS profileThumb FROM MBoard AS article '+
+  'JOIN users AS b ON(article.writer_id=users.uid) ' + whereSql1 + orderData + offsetSql + ' ORDER BY article.created_at DESC LIMIT 10';
 
   conn.query(sql, areaNoArray, function(err, result, fields){
     if(err){
