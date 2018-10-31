1591535186392663257e57247e2f32b3730a0f71
sunghyunpark <sunghyun1038@naver.com>
2018-10-31 16:01:15 +0900

    매칭 게시글 수정 api

--- routes/boards.js
+++ routes/boards.js
@@ -128,6 +128,12 @@ router.put('/edit', function(req, res){
       if(err){
         console.log(err);
         res.json(responseUtil.successFalse(500, 'Internal Server Error'));
+      }else{
+        var sql = 'UPDATE '+tableName+' SET title=?, contents=?, match_date=?, average_age=?, charge=?, play_rule=? WHERE no=?';
+        conn.query(sql, [title, contents, matchDate, averageAge, charge, playRule, no], function(err, result, fields){
+          res.json(err ? responseUtil.successFalse(500, 'Internal Server Error') : responseUtil.successTrue('Success'));
+          return;
+        })
       }
     })
   }
